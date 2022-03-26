a3b8463159d453bc96f6f5482900b5254851e08f
Darshan Tarsariya <darshantarsariya17@gmail.com>
2022-03-26 10:46:51 +0530

    first

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+const dots = document.querySelectorAll(".dot-container button");
+const images = document.querySelectorAll(".image-container img");
+// current slide
+let i = 0; 
+// total slides
+let j = 3; 
+
+
+//next slider button
+function next()
+{
+    document.getElementById("content" + (i+1)).classList.remove("active");
+    i = ( j + i + 1) % j;
+    document.getElementById("content" + (i+1)).classList.add("active");
+    indicator( i+ 1 );
+}
+//previous slider button
+function prev()
+{
+    document.getElementById("content" + (i+1)).classList.remove("active");
+    i = ( j + i + 1) % j;
+    document.getElementById("content" + (i+1)).classList.add("active");
+    indicator( i+ 1 );
+     
+}
+// display current indicator for dot
+function indicator(num){
+    dots.forEach(function(dot){
+        dot.style.backgroundColor = "transparent";
+    });
+    document.querySelector(".dot-container button:nth-child(" + num + ")").style.backgroundColor = "#DD4A48";
+}
+// display the image of perticular dot
+function dot(index){
+    images.forEach(function(image){
+        image.classList.remove("active");
+    });
+    document.getElementById("content" + index).classList.add("active");
+    i = index - 1;
+    indicator(index);
+}
